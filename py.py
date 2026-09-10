b02efc6005d8be175bf6067628483290a564724e
gwanhu lee <t3rx2orist@kyonggi.ac.kr>
2026-09-10 15:56:03 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,3 +1,4 @@
+"""
 import time
 
 ch, msg = input("문자:"), input("메시지:")
@@ -11,4 +12,19 @@ print(line)
 print(front + msg + back)
 print(front + MSG + back)
 print(line)
+"""
 
+ch = input("문자:")
+msg = input("메시지:")
+MSG = msg.upper()
+margin = int(input('여백:'))
+n = len(msg)
+loop = int(input('반복횟수:'))
+line = str(ch) * ((n + margin * 2 + 2) * loop - (loop-1))
+front = str(ch) + ' ' * margin
+back = ' ' * margin 
+ 
+print(line)
+print((front + msg + back) * loop + str(ch))
+print((front + MSG + back) * loop + str(ch))
+print(line)
